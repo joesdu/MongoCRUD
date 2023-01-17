8818cfe912b56688363c4c2f999b166571b88c47
Joes <dygood@outlook.com>
2023-01-18 00:06:36 +0800

    更新MongoDB数组的一些常用操作.

--- MongoCRUD/Controllers/MongoArrayController.cs
+++ MongoCRUD/Controllers/MongoArrayController.cs
@@ -12,8 +12,7 @@ public class MongoArrayController : ControllerBase
     {
         _db = db;
     }
-
-    private readonly FilterDefinitionBuilder<FamilyInfo> _bf = Builders<FamilyInfo>.Filter;
+    
     private readonly UpdateDefinitionBuilder<FamilyInfo> _bu = Builders<FamilyInfo>.Update;
 
     [HttpPost("Init")]
@@ -24,7 +23,7 @@ public class MongoArrayController : ControllerBase
             Name = "野比家",
             Members = new()
             {
-                new() 
+                new()
                 {
                     Id = ObjectId.GenerateNewId().ToString(),
                     Index = 0,
@@ -64,5 +63,74 @@ public class MongoArrayController : ControllerBase
         };
         await _db.FamilyInfo.InsertOneAsync(obj);
         return obj;
+    }
+
+    [HttpPost("Create")]
+    public async Task<Person> AddOneElement()
+    {
+        var dorami = new Person
+        {
+            Id = ObjectId.GenerateNewId().ToString(),
+            Index = 4,
+            Name = "哆啦美",
+            Age = 1,
+            Gender = Gender.女,
+            Birthday = DateOnly.ParseExact("2114-12-02", "yyyy-MM-dd")
+        };
+        _ = await _db.FamilyInfo.UpdateOneAsync(c => c.Name == "野比家", _bu.Push(c => c.Members, dorami));
+        return dorami;
+    }
+
+    [HttpPut("UpdateOne")]
+    public async Task UpdateOneElement()
+    {
+        // 这里我们举得例子是将哆啦美的名字变更为日文名字.
+        // 这里我们假设查询参数同样是通过参数传入的,所以我们写出了如下代码.
+        _ = await _db.FamilyInfo.UpdateOneAsync(
+            c => c.Name == "野比家" & c.Members.Any(s => s.Index == 4),
+            _bu.Set(c => c.Members[-1].Name, "ドラミ"));
+    }
+
+    [HttpDelete("DeleteOne")]
+    public async Task DeleteOneElement()
+    {
+        _ = await _db.FamilyInfo.UpdateOneAsync(
+            c => c.Name == "野比家",
+            _bu.PullFilter(c => c.Members, f => f.Index == 4));
+    }
+
+    [HttpGet("OneElement")]
+    public async Task<Person> GetOneElement()
+    {
+        //var sql = _db.FamilyInfo.Find(c => c.Name == "野比家")
+        //    .Project(c => c.Members.First()).ToString();
+        return await _db.FamilyInfo.Find(c => c.Name == "野比家")
+            .Project(c => c.Members.First()).SingleOrDefaultAsync();
+        //return await _db.FamilyInfo.Find(c => c.Name == "野比家")
+        //    .Project(c => c.Members.Find(s => s.Name == "哆啦A梦")).SingleOrDefaultAsync();
+    }
+
+    [HttpGet("AllElement")]
+    public async Task<IEnumerable<Person>> GetAllElement()
+    {
+        return await _db.FamilyInfo.Find(c => c.Name == "野比家")
+            .Project(c => c.Members).SingleOrDefaultAsync();
+    }
+
+    [HttpGet("Unwind")]
+    public async Task<dynamic> GetUnwind()
+    {
+        // Project中的UnwindObj我们往往使用子类,这样可以将一些不必要的数据屏蔽或者丢弃
+        var query = _db.FamilyInfo.Aggregate().Match(c => c.Name == "野比家")
+            .Project(c => new UnwindObj<List<Person>>
+            {
+                Obj = c.Members,
+                Count = c.Members.Count
+            })
+            .Unwind(c => c.Obj, new AggregateUnwindOptions<UnwindObj<Person>> { IncludeArrayIndex = "Index" });
+        //var sql = query.ToString();
+        var total = query.Count().FirstOrDefaultAsync().Result.Count;
+        var list = await query.Skip(2).Limit(1).ToListAsync();
+        return new Tuple<long?, List<UnwindObj<Person>>>(total, list);
     }
 }
---
+++ MongoCRUD/UnwindObj.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoCRUD;
+
+public class UnwindObj<T>
+{
+    /// <summary>
+    /// 1.T as List,use for Projection,
+    /// 2.T as single Object,use for MongoDB array field Unwind result
+    /// </summary>
+    [BsonElement("Obj")]
+    public T? Obj { get; set; }
+    /// <summary>
+    /// when T as List,record Count
+    /// </summary>
+    [BsonElement("Count")]
+    public int Count { get; set; }
+    /// <summary>
+    /// record array field element's index before Unwinds
+    /// </summary>
+    [BsonElement("Index")]
+    public int Index { get; set; }
+}
