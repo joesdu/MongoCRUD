421e7c16fa2e477416f7826783d59963c43d79cb
杜宇 <dygood@outlook.com>
2023-01-19 15:10:12 +0800

    添加索引的创建和删除例子

---
+++ MongoCRUD/Controllers/IndexesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoCRUD.Models;
+using MongoDB.Driver;
+
+namespace MongoCRUD.Controllers;
+[Route("api/[controller]"), ApiController]
+public class IndexesController : ControllerBase
+{
+    private readonly DbContext _db;
+    public IndexesController(DbContext db)
+    {
+        _db = db;
+    }
+
+    [HttpPut("CreateIndexes")]
+    public async Task CreateIndexes()
+    {
+        var indexModel = new CreateIndexModel<Cat>(
+            Builders<Cat>.IndexKeys.Ascending(c => c.Id).Descending(c => c.No),
+            new CreateIndexOptions
+            {
+                Name = "Id 1 No -1",
+                Background = true
+            });
+        _ = await _db.Cat.Indexes.CreateOneAsync(indexModel);
+    }
+
+    [HttpPut("DeleteIndexes")]
+    public async Task DeleteIndexes()
+    {
+        await _db.Cat.Indexes.DropOneAsync("Id 1 No -1");
+    }
+}
